6b8f02c203fb0fc110884c2691525ba013c85517
manas <manas1.mishra@ril.com>
2019-04-06 16:38:30 +0530

    InvitationHolderview added and changed

--- app/src/main/java/com/example/newphoto/MainActivity.kt
+++ app/src/main/java/com/example/newphoto/MainActivity.kt
@@ -2,18 +2,32 @@ package com.example.newphoto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v4.view.GestureDetectorCompat
+import android.support.v4.view.MotionEventCompat
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.LayoutDirection
+import android.view.MotionEvent
+import android.view.GestureDetector
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
     private lateinit var photoRecyclerView: RecyclerView
     private lateinit var photoAdapter: PhotoAdapter
     private lateinit var photoRecycleManager: RecyclerView.LayoutManager
 
+    private lateinit var mDetector: GestureDetectorCompat
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        configureRecyclerView()
+        //configureRecyclerView()
+        mDetector = GestureDetectorCompat(this, this)
     }
 
     fun configureRecyclerView() {
@@ -25,9 +39,61 @@ class MainActivity : AppCompatActivity() {
         photoArr.add(R.drawable.flower5)
 
         photoAdapter = PhotoAdapter(this, photoArr)
+        photoRecycleManager = LinearLayoutManager(this, 0, false)
         photoRecyclerView = findViewById<RecyclerView>(R.id.photoRecyclerView).apply {
             layoutManager = photoRecycleManager
             adapter = photoAdapter
         }
     }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (mDetector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        if (e != null) {
+            if (e.actionMasked == MotionEvent.ACTION_DOWN) {
+                print("Down")
+                showInvitationView()
+            } else if (e.actionMasked == MotionEvent.ACTION_UP) {
+                print("UP")
+            }
+        }
+
+        return true
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        return true
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return true
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return true
+    }
+
+
+    fun showInvitationView() {
+        val layoutParams = invitationHoderview.layoutParams as? ViewGroup.MarginLayoutParams
+        layoutParams?.topMargin = 50
+        invitationHoderview.requestLayout()
+
+    }
+
+
+
 }
